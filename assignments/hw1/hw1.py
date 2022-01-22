37c08cffc3a7537b359459bca6cc56f351f53d6a
spencerhoag <hoags@g.cofc.edu>
2022-01-21 22:12:56 -0500

    Completed hw1.

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,35 +1,43 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Spencer Hoag>
+<hw1>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
+Problem: <All lines of code are intended to take the user input and use it to calculate and display
+the area of a rectangle, volume, a basketball player's shooting percentage, a customer's total cost
+of shipped coffee, and the conversion from kilometers to miles.>
 Certification of Authenticity:
-<include one of the following>
+<I certify that this assignment is entirely my own work.>
 I certify that this assignment is entirely my own work.
 I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
-
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 def calc_volume():
-    pass
-
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("Volume =", volume)
 def shooting_percentage():
-    pass
-
-
+    fgm = eval(input("Enter the player's total shots: "))
+    fga = eval(input("Enter how many shots the player made: "))
+    fgp = (fgm / fga) * 100
+    print("Shooting Percentage: ", fgp, "%")
 def coffee():
-    pass
-
-
+    pounds_ordered = eval(input("How many pounds of coffee would you like? "))
+    cost_per_pound = 10.50
+    shipping = 0.86
+    order_fee = 1.50
+    total_cost = (pounds_ordered * cost_per_pound) + (shipping * pounds_ordered) + order_fee
+    print("You're total is: ", total_cost)
 def kilometers_to_miles():
-    pass
-
-
+    km = eval(input("How many Kilometers did you travel? "))
+    miles = km * 0.62137
+    print("That's", miles, "miles!")
 if __name__ == '__main__':
     pass
