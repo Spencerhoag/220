d6abd08959a9cd42ec4d374f8accf7de224a4364
spencerhoag <hoags@g.cofc.edu>
2022-02-04 23:56:49 -0500

    completed hw 3.

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,56 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Spencer Hoag>
+<hw3>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <All programs use for loops to obtain user input and display the average grade of students, the total amount
+tipped across five people, the square root of a number, and a sequence with steps by 2 and repeats every number twice,>
 
 Certification of Authenticity:
-<include one of the following>
+<I certify that this assignment is entirely my own work.>
 I certify that this assignment is entirely my own work.
 I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def average():
-    pass
+    total_grades = eval(input("How many grades will you enter? "))
+    acc = 0
+    avg = 0
+    for _ in range(total_grades):
+        acc = eval(input("Enter grade: "))
+        avg = avg + acc / total_grades
+    print("Average is:", avg)
 
 
 def tip_jar():
-    pass
+    total = 0
+    for _ in range(5):
+        tip = eval(input("How much would you like to donate? "))
+        total = total + tip
+    print("Total tips: ", total)
 
 
 def newton():
-    pass
+    root = eval(input("What number do you want to square root? "))
+    improve = eval(input("How many times should we improve the approximation? "))
+    approx = 1
+    for _ in range(improve):
+        approx = (approx + root / approx) / 2
+    print("The square root is approximately", approx)
 
 
 def sequence():
-    pass
+    terms = eval(input("How many terms would you like? "))
+    for _ in range(1, terms + 1, 2):
+        for i in range(2):
+            print(_, end="\t")
 
 
 def pi():
-    pass
+    terms = eval(input("How many terms in the series? "))
+    acc = 1
+
+
 
 
 if __name__ == '__main__':
