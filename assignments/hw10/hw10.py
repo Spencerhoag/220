3d1d84286f2253ab5f9dfc1a959e6c4b9e217042
spencerhoag <hoags@g.cofc.edu>
2022-04-08 23:30:40 -0400

    completed hw 10.

---
+++ assignments/hw10/hw10.py
@@ -0,0 +1,66 @@
+"""
+hw10.py
+"""
+
+
+def fibonacci(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n > 1:
+        first = 1
+        second = 1
+        mylist = [first, second]
+        i = 2
+        while i < n:
+            next_term = first + second
+            mylist.append(next_term)
+            first = second
+            second = next_term
+            i = i + 1
+        return second
+    else:
+        return None
+
+
+def double_investment(principle, rate):
+    years = 0
+    total = principle
+    while total <= principle * 2:
+        total = total + total * rate
+        years = years + 1
+    return years
+
+
+def syracuse(n):
+    total = [n]
+    while n != 1:
+        if n % 2 == 0:
+            even = n / 2
+            total.append(even)
+            n = even
+        else:
+            odd = 3 * n + 1
+            total.append(odd)
+            n = odd
+    return total
+
+
+def primes(n):
+    i = 2
+    mylist = []
+    while i < n:
+        if i % n == 0:
+            i = i + 1
+        else:
+            mylist.append(i)
+            i = i + 1
+    return mylist
+
+
+def goldbach(n):
+    if n % 2 != 0:
+        return None
+    else:
+        pass
---
+++ assignments/hw10/sphere.py
@@ -0,0 +1,12 @@
+import math
+class Sphere:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_radius(self):
+
+
+    def surface_area(self):
+
+
+    def volume(self):
